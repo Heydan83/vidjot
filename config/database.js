80de77a5016d94014f187ab6e87ebf5aa4d94cf4
adan.ulloa <adan.ulloa@gmail.com>
2020-03-01 19:28:19 -0600

    changes to the database.js file

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.export = { mongoURI: 'mongodb+srv://adan:adan@cluster0-kmqfm.mongodb.net/test?retryWrites=true&w=majority' }
+  module.exports = { mongoURI: 'mongodb+srv://adan:adan@cluster0-kmqfm.mongodb.net/test?retryWrites=true&w=majority' }
 } else {  
   module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
 }
